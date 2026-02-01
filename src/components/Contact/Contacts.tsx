1178eb63fe475eafa3997c5ed96390248aee0400
Fariba-Mirzaie <faribamirzaie@yahoo.com>
2026-02-01 13:24:14 +0330

    add useTransition

--- src/components/Contact/Contacts.tsx
+++ src/components/Contact/Contacts.tsx
@@ -8,6 +8,7 @@ import {
   useMemo,
   useRef,
   useState,
+  useTransition,
 } from "react";
 import { getAllContacts } from "../../services/contactService";
 import { Contact } from "./contact.type";
@@ -20,11 +21,35 @@ export default function Contacts() {
   const newContactRef = useRef<Record<string, HTMLDivElement | null>>({});
   // if (isFirstRender.current) isFirstRender.current = false; in useeffect
 
+  const [isPending, startTransition] = useTransition();
+  const [filteredResult, setFilteredResult] = useState<Contact[]>([]);
+
   const deferredSearch = useDeferredValue(searchText);
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    startTransition(() => {
+      // const start = performance.now();
+      // while (performance.now() - start < 800) {}
+
+      let count = 0;
+      while (count === 30000) count++;
+
+      if (!deferredSearch) setFilteredResult(contacts);
+
+      if (deferredSearch) {
+        const result = contacts.filter(
+          (f) =>
+            f.name.includes(deferredSearch) ||
+            f.family.includes(deferredSearch),
+        );
+        setFilteredResult(result);
+      }
+    });
+  }, [deferredSearch]);
 
   useLayoutEffect(() => {
     if (contacts.length > 0) {
@@ -38,45 +63,38 @@ export default function Contacts() {
     }
   }, [contacts]);
 
-  const filteredContacts = useMemo(() => {
-    if (!deferredSearch) return contacts;
-
-    if (deferredSearch) {
-      return contacts.filter(
-        (f) =>
-          f.name.includes(deferredSearch) || f.family.includes(deferredSearch),
-      );
-    }
-  }, [contacts, deferredSearch]);
-
-  const contactsCount = useMemo(() => {
-    return filteredContacts?.length;
-  }, [filteredContacts]);
+  // const contactsCount = useMemo(() => {
+  //   return filteredContacts?.length;
+  // }, [filteredContacts]);
 
   async function fetchData() {
     setLoading(true);
-    setContacts(await getAllContacts());
+    const allContacts = await getAllContacts();
+    setContacts(allContacts);
+    setFilteredResult(allContacts);
     setLoading(false);
   }
 
   return (
     <>
+      {isPending && <h5>در حال فیلتر </h5>}
+
       {isFirstRender.current && (
         <h1 style={{ color: "green", fontSize: 36, fontWeight: 600 }}>
           Welcome to contact app
         </h1>
       )}
-      <h4 style={{ color: "blue" }}> تعداد کل مخاطبین:{contactsCount} </h4>
+      {/* <h4 style={{ color: "blue" }}> تعداد کل مخاطبین:{contactsCount} </h4> */}
       <Link to="/contact/add">
         <button className="btn">ایجاد مخاطب</button>
       </Link>
       <div className="contacts">
         {loading ? (
           <Spinner />
-        ) : filteredContacts && filteredContacts.length > 0 ? (
-          filteredContacts.map((contact, index) => (
+        ) : filteredResult && filteredResult.length > 0 ? (
+          filteredResult.map((contact, index) => (
             <div
-              key={index}
+              key={contact.id}
               ref={(el) => {
                 newContactRef.current[contact.id] = el;
               }}
